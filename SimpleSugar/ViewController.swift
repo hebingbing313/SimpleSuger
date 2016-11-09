b97bfe1d9d432e045acb76979b11efff220459e6
2016 <a2016@2016demacbook-pro.local>
2016-11-09 14:59:41 +0800

    Initial Commit

---
+++ SimpleSugar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSugar
+//
+//  Created by 2016 on 16/11/9.
+//  Copyright © 2016年 2016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
